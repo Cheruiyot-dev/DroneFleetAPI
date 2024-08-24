80b880bc5bac73ce2abcd5a1a1a2e31a5d427082
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-08-24 21:25:16 +0300

    configure pagination classes

--- DroneFleetAPI/settings.py
+++ DroneFleetAPI/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+ 'DEFAULT_PAGINATION_CLASS':
+ 'rest_framework.pagination.LimitOffsetPagination',
+ 'PAGE_SIZE': 10
+ }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
